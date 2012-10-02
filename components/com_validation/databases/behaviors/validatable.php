17c74fa589e9fc3c5a6053b6de345197b4b5bf85
Oli Griffiths <oli@organic-development.com>
2012-10-02 22:31:53 +0100

    Adding behavior methods

--- components/com_validation/databases/behaviors/validatable.php
+++ components/com_validation/databases/behaviors/validatable.php
@@ -7,21 +7,57 @@
 
 class ComValidationDatabaseBehaviorValidatable extends KDatabaseBehaviorAbstract
 {
+
 	protected function _beforeTableInsert(KCommandContext $context)
+	{
+		return $this->_beforeTableUpdate($context);
+	}
+
+	protected function _beforeTableUpdate(KCommandContext $context)
 	{
 		if($context->data instanceof KDatabaseRowAbstract)
 		{
 			if($context->data->isValidatable())
 			{
-				return $context->data->validate();
+				return $context->data->validateData();
 			}
 		}
 	}
 
-	public function validate()
+	public function validateData()
 	{
+		$mixer = $this->getMixer();
+		if(!$mixer instanceof KDatabaseRowAbstract)
+		{
+			throw new KDatabaseException(__FUNCTION__.' may only be called on a KDatabaseRow');
+		}
 
+		$rules = $mixer->getValidationRules();
+
+		var_dump($rules);
 
 		exit('validate');
+	}
+
+
+	public function getValidationRules()
+	{
+		$mixer = $this->getMixer();
+		if(!$mixer instanceof KDatabaseRowAbstract)
+		{
+			throw new KDatabaseException(__FUNCTION__.' may only be called on a KDatabaseRow');
+		}
+
+		$rules = array();
+		$columns = $mixer->getTable()->getColumns();
+		foreach($columns AS $id => $column)
+		{
+			if($column->required)
+			{
+				$rules[$id] = array('required');
+			}
+		}
+
+		return $rules;
 	}
 }
