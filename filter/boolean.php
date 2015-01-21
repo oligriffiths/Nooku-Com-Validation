f0709c48cda356a195f64264067494b80a3dfad4
Oli Griffiths <oli.griffiths@expandtheroom.com>
2015-01-21 14:47:39 -0500

    Add boolean initialize method

--- filter/boolean.php
+++ filter/boolean.php
@@ -19,6 +19,23 @@ class FilterBoolean extends Library\FilterBoolean
 		$this->_strict = $config->strict;
 	}
 
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param  ObjectConfig $config An optional ObjectConfig object with configuration options
+     * @return void
+     */
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array(
+            'strict' => false,
+        ));
+
+        parent::_initialize($config);
+    }
+
 
 	/**
 	 * Validate the value is boolean.
